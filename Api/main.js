c152206ac880ca6a2d1d2adfed73e69431c83d3f
PaPguS <zhitnikov2009yan@mail.ru>
2025-03-22 19:10:32 +0300

    logn

---
+++ Api/main.js
@@ -0,0 +1,37 @@
+let Search = document.getElementById('Search');
+let SearchBtn = document.getElementById('SearchBtn');
+let main = document.getElementById('main');
+let spiner = document.getElementById("spiner");
+
+let dataCard;
+
+SearchBtn.addEventListener('click', getCard)
+
+async function getCard() {
+    spiner.classList.remove('none')
+    let response = await fetch(`https://api.github.com/users/${Search.value}`)
+    if (response.ok){
+        dataCard = await response.json()
+        generateCard()
+    }else{
+        console.log('Данные не верные')
+    }
+    spiner.classList.add('none')
+}
+
+function generateCard(){
+    main.classList.remove('none')
+    main.innerHTML = `        
+        <img src="https://avatars.mds.yandex.net/i?id=2d05f27b48facbd406fdd2f6fca23ad3723de632-5286004-images-thumbs&n=13"/>
+        <h1>Login</h1>
+        <p>description</p>
+        <div>
+            <i class="fab fa-js"></i>
+            <span>Репрезиториев: 5</span>
+        </div>
+        <div>
+            <i class="fab fa-python"></i>
+            <span>Репрезиториев: 6</span>
+        </div>
+        `
+}
